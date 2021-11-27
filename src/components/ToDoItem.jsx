76d017b66efb0b3070c56f40720cefb5cb9ec7fc
Summer <72228259+SummerinTech@users.noreply.github.com>
2021-11-27 15:40:48 -0800

    add cursor to li item

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -3,6 +3,7 @@ import React from "react";
 export default function ToDoItem(props) {
   return (
     <li
+      style={{ cursor: "pointer" }}
       onClick={() => {
         props.onChecked(props.id);
       }}
